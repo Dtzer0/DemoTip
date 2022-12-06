8c3e088c534566736ccb82148fa1079f5327bdda
Dtzer0 <dotzero@kakao.com>
2022-12-06 13:43:59 +0900

    Initial Commit

---
+++ StarOption/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StarOption
+//
+//  Created by 홍태희 on 2022/12/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StarOption/StarOptionApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarOptionApp.swift
+//  StarOption
+//
+//  Created by 홍태희 on 2022/12/06.
+//
+
+import SwiftUI
+
+@main
+struct StarOptionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
